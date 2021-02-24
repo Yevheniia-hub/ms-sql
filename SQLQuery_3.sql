d35084a14ace40269a729f6117491d06a44d07ac
Yevheniia Lytvynenko <57947875+Yevheniia-hub@users.noreply.github.com>
2021-02-24 09:28:20 +0200

    Create SQLQuery_3.sql
    test

---
+++ SQLQuery_3.sql
@@ -0,0 +1,28 @@
+select c.id as customer_id, c.name as customer_name
+     ,o.slide_order_summ
+     ,p.paymet
+
+  from customers c
+  join (
+         select oo.customerId
+         ,oo.id
+         ,oo.summa
+         , v.slide_order_summ
+           from orders oo
+          cross apply 
+                (
+                  select sum(summa) as slide_order_summ
+                    from orders o1
+                  where oo.customerId = o1.customerId
+                ) v
+       )o
+      on c.id = o.customerId
+  join payments p
+    on c.id = p.customerId
+
+ where o.slide_order_summ <= p.paymet
+group by c.id,c.name,o.slide_order_summ,p.paymet
+
+
+ 
+
